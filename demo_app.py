01ab0212ce6ee749b06343f551df2bf1f1195229
harbarsem <harbarsem@gmail.com>
2021-04-04 13:15:33 +0300

    test

--- demo_app.py
+++ demo_app.py
@@ -20,7 +20,7 @@ with st.echo(code_location='below'):
     geo_states['name'] = geo_states['name'].apply(lambda x: x.upper())
     geo_states = geo_states[['name', 'geometry']]
     geo_states["rp"] = geo_states['geometry'].representative_point()
-    geo_states['ctr'] = geo_states['geometry'].to_crs({'init': 'epsg:3395'}).centroid
+    #geo_states['ctr'] = geo_states['geometry'].to_crs({'init': 'epsg:3395'}).centroid
     df = geo_states.merge(data, how="right", left_on="name", right_on="state")
     df['area'] = df['geometry'].to_crs({'init': 'epsg:3395'}).map(lambda
                                                                       p: p.area / 10 ** 6)  # (copy from https://gis.stackexchange.com/questions/218450/getting-polygon-areas-using-geopandas)
@@ -29,29 +29,27 @@ with st.echo(code_location='below'):
     st.write(f"Вы выбрали: {selected_year!r}")
 
     dict_col = {'DEMOCRAT': "Blues", 'REPUBLICAN': "Reds", "LIBERTARIAN": "Oranges"}
-    a=selected_year
-    for b in ['DEMOCRAT', 'REPUBLICAN', "LIBERTARIAN"]:
-        sample = df[(df["year"] == a) & (df["party_simplified"] == b)]
-        plot1 = plt.figure()
-        if b == "LIBERTARIAN":
-            d = 0.05
-        else:
-            d = 1
-        sample.plot(column='percentage', norm=mpl.colors.Normalize(vmin=0, vmax=d), figsize=(25, 15), legend=True,
-                    cmap=dict_col[b])
-        plt.xlim(-130, -65)
-        plt.ylim(20, 55)
-        title = '{} candidate: {}, elections in {}'.format(b.lower(), df[
-            (df["year"] == a) & (df["party_simplified"] == b)]['candidate'].unique()[0], a)
-        mpl.pyplot.title(title, fontsize=20, fontweight='bold', loc='center')
-        for x, y, label in zip(sample['ctr'].x - 1.5, sample['ctr'].y, sample["state"]):
-            if label != 'ALASKA' and label != 'HAWAII' and (sample[sample['state'] == label]['area'] > 40000).to_list()[
-                0]:
-                if label == "MISSISSIPPI":
-                    plt.text(x, y + 0.7, label, fontsize=8, color='black', alpha=1, weight="bold")
-                else:
-                    plt.text(x, y, label, fontsize=8, color='black', alpha=1, weight="bold")
-        st.pyplot(plot1)
+    a=int(selected_year)
+
+    b = 'REPUBLICAN'
+    sample = df[(df["year"] == a) & (df["party_simplified"] == b)]
+    sample.plot(column='percentage', norm=mpl.colors.Normalize(vmin=0, vmax=1), figsize=(25, 15), legend=True,
+                cmap="Reds",
+                legend_kwds={'label': "Share of votes"})
+    plt.xlim(-130, -65)
+    plt.ylim(20, 55)
+    title = 'Elections {}: {} candidate - {}'.format(a, df[(df["year"] == 2020) & (df["party_simplified"] == b)][
+        'candidate'].unique()[0], b)
+    mpl.pyplot.title(title, fontsize=30, fontweight='bold', loc='center')
+    for x, y, label in zip(sample['rp'].x - 1.5, sample['rp'].y, sample["state"]):
+        if label != 'ALASKA' and label != 'HAWAII' and (sample[sample['state'] == label]['area'] > 40000).to_list()[0]:
+            if label == "MISSISSIPPI" or label == 'VERMONT':
+                plt.text(x, y + 0.7, label, fontsize=8, color='black', alpha=1, weight="bold")
+            elif label == "IDAHO":
+                plt.text(x + 1, y - 1, label, fontsize=8, color='black', alpha=1, weight="bold")
+            else:
+                plt.text(x, y, label, fontsize=8, color='black', alpha=1, weight="bold")
+        st.pyplot()
 
     x = np.linspace(0, 10, 500)
     fig = plt.figure()
